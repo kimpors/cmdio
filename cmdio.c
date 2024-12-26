f4ac2b73bdfa1cf0c8e9d295888cf7c8d11fb6fd
kimpors <mikita_korshak@ukr.net>
2024-12-26 14:42:16 +0200

    Add kbhit function

--- cmdio.c
+++ cmdio.c
@@ -4,6 +4,46 @@
 extern "C" {
 #endif
 
+#ifdef __linux__
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int cokbhit(void)
+{
+	int ch, oldf;
+	struct termios oldt, newt;
+
+	tcgetattr(STDIN_FILENO, &oldt);
+	newt = oldt;
+	newt.c_lflag &= ~(ICANON | ECHO);
+	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+	oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+	fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+	ch = getchar();
+
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+	fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+	if (ch != EOF)
+	{
+		ungetc(ch, stdin);
+		return 1;
+	}
+
+	return 0;
+}
+#elif _WIN32
+#include <conio.h>
+
+int cokbhit(void)
+{
+	reutrn kbhit();
+}
+#endif
+
 inline void coclear(void)
 {
 	printf("\e[1;1H\e[2J");
--- cmdio.h
+++ cmdio.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <stdio.h>
 
+int cokbhit(void);
 inline void coclear(void);
 inline void comove(size_t row, size_t col);
