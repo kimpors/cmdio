aafc42459f4ec68de3f5a8dc34a9829be18bcf5a
kimpors <mikita_korshak@ukr.net>
2025-06-19 10:17:05 +0300

    Change color defined names
    just add prefix CO_
    
    Signed-off-by: kimpors <mikita_korshak@ukr.net>

--- cmdio.h
+++ cmdio.h
@@ -10,14 +10,15 @@
 #endif
 
 typedef enum {
-	BLACK = 30,
-	RED = 31,
-	GREEN = 32,
-	YELLOW = 33,
-	BLUE = 34,
-	PINK = 35,
-	CYAN = 36,
-	WHITE = 37,
+	CO_RESET,
+	CO_BLACK = 30,
+	CO_RED = 31,
+	CO_GREEN = 32,
+	CO_YELLOW = 33,
+	CO_BLUE = 34,
+	CO_PINK = 35,
+	CO_CYAN = 36,
+	CO_WHITE = 37,
 } CO_Color;
 
 EXPORT int cokbhit(void);
