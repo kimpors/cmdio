0d5f6d43616b0ebb7ae74e00f4a52dc4f7a67a96
kimpors <mikita_korshak@ukr.net>
2025-07-31 17:20:13 +0300

    Add macro for prefix
    If i ever wanted to change prefix for lib
    I can just change two macros
    
    Signed-off-by: kimpors <mikita_korshak@ukr.net>

--- cmdio.h
+++ cmdio.h
@@ -1,30 +1,33 @@
 #pragma once
 #include <stdio.h>
 
+#define PREFIX(name) CO_##name 
+#define SPREFIX(name) co_##name // small prefix
+
 #ifdef _WIN32
 	#define EXPORT __declspec(dllexport)
 #elif __linux__
 	#include <sys/ioctl.h>
 	#define EXPORT
-	EXPORT void cosize(struct winsize *size);
+	EXPORT void SPREFIX(size)(struct winsize *size);
 #endif
 
 typedef enum {
-	CO_RESET,
-	CO_BLACK = 30,
-	CO_RED = 31,
-	CO_GREEN = 32,
-	CO_YELLOW = 33,
-	CO_BLUE = 34,
-	CO_PINK = 35,
-	CO_CYAN = 36,
-	CO_WHITE = 37,
-} CO_Color;
+	PREFIX(RESET),
+	PREFIX(BLACK),
+	PREFIX(RED),
+	PREFIX(GREEN),
+	PREFIX(YELLOW),
+	PREFIX(BLUE),
+	PREFIX(PINK),
+	PREFIX(CYAN),
+	PREFIX(WHITE)
+} PREFIX(Color);
 
-EXPORT int cokbhit(void);
-EXPORT int cogetch(void);
-EXPORT void coclear(void);
-EXPORT void cobgcolor(CO_Color color);
-EXPORT void cofgcolor(CO_Color color);
-EXPORT void cocolor(CO_Color fg, CO_Color bg);
-EXPORT void comove(size_t row, size_t col);
+EXPORT int SPREFIX(kbhit)(void);
+EXPORT int SPREFIX(getch)(void);
+EXPORT void SPREFIX(clear)(void);
+EXPORT void SPREFIX(bgcolor)(PREFIX(Color) color);
+EXPORT void SPREFIX(fgcolor)(PREFIX(Color) color);
+EXPORT void SPREFIX(color)(PREFIX(Color) fg, PREFIX(Color) bg);
+EXPORT void SPREFIX(move)(size_t row, size_t col);
